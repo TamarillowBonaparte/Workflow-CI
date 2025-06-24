2a9229f224f785b87d66acf8f154249a2ecc2546
MOH. DANI K. S <125776654+TamarillowBonaparte@users.noreply.github.com>
2025-06-24 13:24:07 +0700

    Update processing.py

--- MLProject/processing.py
+++ MLProject/processing.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-input_path = "MLProject/heartdataset/heart.csv"
+input_path = "heartdataset/heart.csv"
 df = pd.read_csv(input_path)
 
 X = df.drop("condition", axis=1)
